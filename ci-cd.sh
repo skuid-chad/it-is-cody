06bf4f27ef0c4358b84026b1ca30b2e73cdbc0ec
skuid-chad <chad.owens@skuid.com>
2019-07-26 08:34:42 -0700

    Add ci-cd.sh script

---
+++ ci-cd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Install npm dependencies
+npm ci
+
+# Install skuid CLI
+wget -q -O ./node_modules/.bin/skuid https://github.com/skuid/skuid/releases/download/0.3.8/skuid_linux_amd64
+chmod +x ./node_modules/.bin/skuid
+
+# deploy skuid metadata to the target skuid site
+./node_modules/.bin/skuid deploy -d skuid-data
+
+# Run tests
+npm test
